bbe8621bea99b65b44830d2419eaf1f9e8b5a6d6
Harmony Burke <theharmonyburke@gmail.com>
2023-10-25 19:37:03 -0500

    fixed api issue and added current temperature to the page

--- script.js
+++ script.js
@@ -1,38 +1,50 @@
-var weatherApi="https://api.openweathermap.org/data/2.5/forecast?q=Dallas&appid=9f2db7bcc59386b227ecd49ea3d0414a"
 
 var citySearch=$("#city-search")
 var currentWeather=$("#current-weather")
 var forecast=$("#forecast")
 $(document).ready(function(){
     // everthing in this function runs on page load and when the given parameters are met
-    $("#search-btn").on("click",function(){
-        console.log("search button")
-        // this function will run when the search button is clicked
-        console.log(weatherApi)
-        // checking to make sure the API is working
+    function weatherData(cityName){
+        var weatherApi=`https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&units=imperial&appid=9f2db7bcc59386b227ecd49ea3d0414a`
+        console.log(weatherApi) 
+        
+
         fetch(weatherApi)
-        .then(function(response){
-            return response.json()
-            console.log(response)
-            // this pulls the information from the API and returns it to the webpage
+            .then(function(response){
+                return response.json()
+                
+                // this pulls the information from the API and returns it to the webpage
+                
+            .then(function (data) {
+                    console.log(data);
+                    // this pulls the specified data from the API
+                    currentWeather.empty 
+                    // this clears the page so that mulitple weather point aren't displayed at the same time
+            if (data.list.length>0){
+                var allData=data.list[0].main;
+                var temp=allData.temp;
+                var humid=allData.humidity;
+
+                currentWeather.append($("<h3>").text("Today's Weather:"+ temp + "F"))
+            }       
+                
+                        
+                        
+                    })
+            })
+        }
+        
+        $("#search-btn").on("click", function(){
+            console.log("search button")
+            // this function will run when the search button is clicked
+            var cityName=citySearch.val();
+            // searches the 
+            if(cityName){
+                weatherData(cityName);
+            }
+        })       
             
         })
-        .then(function (data) {
-            console.log(data);
-            // this pulls the specified data from the API
-            for (i=0;i<data.length; i++){
-                var todayWeather=$("<h3>")
-                  todayWeather.textContent=data.list.main.temp;
-
-                  currentWeather.appendChild(todayWeather);
-                  console.log(todayWeather)
-                  
-             };
-
-        })
-
-    })
-})
        
      
         
